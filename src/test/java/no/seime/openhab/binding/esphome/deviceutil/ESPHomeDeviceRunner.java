237c4d52b84ffab3afdee7f98e65397e5c846684
Cody Cutrer <cody@cutrer.us>
2026-04-14 15:31:54 -0600

    fixes to allow emulator test to run in sandbox

--- src/test/java/no/seime/openhab/binding/esphome/deviceutil/ESPHomeDeviceRunner.java
+++ src/test/java/no/seime/openhab/binding/esphome/deviceutil/ESPHomeDeviceRunner.java
@@ -1,6 +1,8 @@
 package no.seime.openhab.binding.esphome.deviceutil;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -10,6 +12,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class ESPHomeDeviceRunner {
+    private static final File TEST_HOME_DIRECTORY = new File("target/test-home");
+    private static final File TEST_PLATFORMIO_CORE_DIRECTORY = new File("target/test-platformio");
+    private static final File TEST_XDG_CACHE_HOME_DIRECTORY = new File("target/test-xdg-cache");
+    private static final File USER_PLATFORMIO_DIRECTORY = new File(System.getProperty("user.home"), ".platformio");
 
     private final Logger logger = LoggerFactory.getLogger(ESPHomeDeviceRunner.class);
 
@@ -22,10 +28,13 @@ public class ESPHomeDeviceRunner {
     }
 
     public void compileAndRun() throws IOException {
-
+        ensureRuntimeDirectories();
         ProcessBuilder compilationBuilder = new ProcessBuilder();
         compilationBuilder.redirectErrorStream(true);
         compilationBuilder.command("esphome", "compile", espDeviceConfigurationYamlFileName.getPath());
+        compilationBuilder.environment().put("HOME", TEST_HOME_DIRECTORY.getAbsolutePath());
+        compilationBuilder.environment().put("PLATFORMIO_CORE_DIR", TEST_PLATFORMIO_CORE_DIRECTORY.getAbsolutePath());
+        compilationBuilder.environment().put("XDG_CACHE_HOME", TEST_XDG_CACHE_HOME_DIRECTORY.getAbsolutePath());
         Consumer<String> compilationListener = s -> {
             logger.info("{}", s);
         };
@@ -76,6 +85,43 @@ public class ESPHomeDeviceRunner {
             emulatorProcess.destroy();
         executorService.shutdown();
         executorService.awaitTermination(5, TimeUnit.SECONDS);
+    }
+
+    private void ensureRuntimeDirectories() throws IOException {
+        createDirectory(TEST_HOME_DIRECTORY);
+        createDirectory(TEST_PLATFORMIO_CORE_DIRECTORY);
+        createDirectory(TEST_XDG_CACHE_HOME_DIRECTORY);
+        linkPlatformIOCache("platforms");
+        linkPlatformIOCache("packages");
+    }
+
+    private void createDirectory(File directory) throws IOException {
+        if (!directory.exists() && !directory.mkdirs()) {
+            throw new IOException("Could not create directory " + directory.getAbsolutePath());
+        }
+    }
+
+    private void linkPlatformIOCache(String directoryName) throws IOException {
+        Path source = USER_PLATFORMIO_DIRECTORY.toPath().resolve(directoryName);
+        Path target = TEST_PLATFORMIO_CORE_DIRECTORY.toPath().resolve(directoryName);
+
+        if (!Files.exists(source)) {
+            return;
+        }
+
+        if (Files.isDirectory(target) && isEmptyDirectory(target)) {
+            Files.delete(target);
+        } else if (Files.exists(target)) {
+            return;
+        }
+
+        Files.createSymbolicLink(target, source);
+    }
+
+    private boolean isEmptyDirectory(Path directory) throws IOException {
+        try (var contents = Files.list(directory)) {
+            return contents.findAny().isEmpty();
+        }
     }
 
     private static class StreamGobbler implements Runnable {
